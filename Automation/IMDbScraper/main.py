f28d6529e4f485085a63ba5b4f74788f7f2ca9e6
NikolayGerogiev <nikolaypavgr@gmail.com>
2020-08-15 16:49:42 +0100

    Runtime+release date

--- Automation/IMDbScraper/main.py
+++ Automation/IMDbScraper/main.py
@@ -1,8 +1,48 @@
 from selenium import webdriver
-import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get('https://www.imdb.com')
 
+TITILE = 'Mulan'
+YEAR = '2020'
 
+try:
+    siteObject = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "suggestion-search")))
+    siteObject.send_keys(TITILE)
+    siteObject.send_keys(Keys.RETURN)
+except:
+    print("search failed")
+
+try:
+    siteObject = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, TITILE)))
+    siteObject.click()
+except:
+    print("failed")
+
+sleep(10)
+
+#Print runtime and release date
+releaseDate = driver.find_elements_by_id("titleDetails")
+for element in releaseDate:
+    elText = element.text
+    if "Release Date" in elText:
+        startIndex = elText.find("Release Date")
+        endindex = elText.find(" See more »")
+        print(elText[startIndex:endindex])
+    if "Runtime" in elText:
+        startIndex = elText.find("Runtime:")
+        endindex = elText.find(" min")
+        print(elText[startIndex:endindex+4])
+
+
+sleep(10)
+driver.quit()
