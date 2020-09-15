f500637268ce40070b35f152ce93d04697f5a133
NikolayGerogiev <nikolaypavgr@gmail.com>
2020-09-15 19:44:14 +0100

    finished

--- Hangman/PyGame Hangman/main.py
+++ Hangman/PyGame Hangman/main.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 
 #setup display
 pygame.init()
@@ -27,12 +28,14 @@ for i in range(7):
 
 #game variables
 hangman_status = 0
-word = "DEVELOPER"
+words = ["DEVELOPER", "PYTHON", "JAVASCRIPT", "GOOGLE", "ABSOLUTE", "ACCEPTED", "ACCURACY", "ACHIEVED", "AGENCIES", "ALTHOUGH", "AUDIENCE", "BIRTHDAY", "BUILDING", "BROTHERS", "CITIZEN", "CLICKING", "CONCEPTS", " CONSTANT", "CONTRACT", "APPLE", "ELEPTHANT", "INVOLVED", " LIFETIME", "WEEKEND", "WARRIORS", "VERTICAL", "SYNDROME", "SPECTRUM", "RESULTED", "ROMANTIC", " PROTOCOL", "PRESENTS", "ORGANIZE", "MOMENTUM", "NAVIGATE", "MAXMIZE", "LITERARY", "INTEGRAL", "HOMEMADE", "GUARDIAN", "EXTENDED", "EMERGING", "DEADLINE", "BOXES", "CLOSE", "FORTH", "HENCE", "INDEX", "JOINT", "JUDGE", "MAJOR", "MONEY", "QUICK", "QUITE", "SEEDS", "SKILLS", "TEAMS", "UNUSUAL", "WATCH", "WHOLE", "WOMEN", "YOUNG", "DOZEN", "FLESH", "ZONES", "EQUIP", "JUICY", "JEWEL", "LIMBS"]
+word = random.randint(0, len(words))
 guessed = []
 
 #fonts
 LETTER_FONT = pygame.font.SysFont('comicsans', 40)
 WORD_FONT = pygame.font.SysFont('comicsans', 60)
+TITLE_FONT = pygame.font.SysFont('comicsans', 80)
 
 #colors
 WHITE = (255,255,255)
@@ -43,8 +46,21 @@ FPS = 60
 clock = pygame.time.Clock()
 run = True
 
+def display_message(message):
+    pygame.time.delay(500)
+    windows.fill(WHITE)
+    text = WORD_FONT.render(message, 1, BLACK)
+    windows.blit(text,(WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(3000)
+    
+
 def draw():
     windows.fill(WHITE)
+
+    #draw title
+    text = TITLE_FONT.render("HANGMAN", 1, BLACK)
+    windows.blit(text, (WIDTH/2 - text.get_width()/2, 20))
 
     #draw words
     display_word = ""
@@ -53,7 +69,8 @@ def draw():
             display_word += letter + " "
         else:
             display_word +="_ "
-    
+    text = WORD_FONT.render(display_word, 1, BLACK)
+    windows.blit(text, (400,200))
 
     #draw buttons
     for letter in letters:
@@ -81,13 +98,27 @@ while run:
                 if visible:
                     distance = math.sqrt((x - m_x)**2 + (y - m_y)**2)
                     if distance < RADIUS:
-                        letter[3] = False     
+                        letter[3] = False    
+                        guessed.append(ltr) 
+                        if ltr not in word:
+                            hangman_status += 1
 
     draw()
 
- 
+    won = True
+    for letter in word:
+        if letter not in guessed:
+            won = False
+            break
+    
+    if won:
+        display_message("You WON")
+        break
 
-                 
-            
+    if hangman_status == 6:
+        display_message("You LOST")
+        break
+
+    
 
 pygame.quit()
